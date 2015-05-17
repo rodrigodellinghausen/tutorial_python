cc112b2d36c67bc1ddd99c2cf7606ecac0425c55
rodrigodellinghausen <dellinghausen@gmail.com>
2015-05-17 20:34:17 -0300

    Tutorial parte 3.

--- first-app/mysite/polls/urls.py
+++ first-app/mysite/polls/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^abc/$', views.abc, name='abcd')
+	url(r'^especific/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	#url(r'^abc/$', views.abc, name='abcd')
 ]
--- first-app/mysite/polls/views.py
+++ first-app/mysite/polls/views.py
@@ -1,7 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 def index (request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+	##return HttpResponse("Hello, world. You're at the polls index.")
+	last_question_list = Question.objects.order_by('-pub_date')[:5]
+	
+	#template = loader.get_template('polls/index.html')
 
-def  abc(request):
-	return HttpResponse("Ola abc")
+	#context = RequestContext(request, {'last_question_list': last_question_list})
+	context = {'last_question_list': last_question_list}
+
+	#return HttpResponse(template.render(context))
+	return render(request, 'polls/index.html', context)
+
+def  detail(request, question_id):
+	#try:
+	#	question = Question.objects.get(pk=question_id)
+	#except Question.DoesNotExist:
+	#	raise Http404("Question does not exist")
+	#return render(request, 'polls/detail.html', {'question': question})
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
